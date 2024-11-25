57e4f1772c736f0d12c4e8260db409cac69bd0c5
dgthaina <thainadagostini61@gmail.com>
2024-11-25 09:17:40 -0300

    Aborting

--- routes/auth.py
+++ routes/auth.py
@@ -4,8 +4,8 @@ auth = Blueprint('auth')
 
 @auth.route('/cadastrar')
 def cadastrar():
-    return render_template('')
+    return render_template('cadastro.html')
 
 @auth.route('/login')
 def login():
-    return render_template('')
+    return render_template('login.html')
